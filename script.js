71b1dc708ad2fdd255a545c3ff4574eb92ce3367
ArpanSankesh <arpansankesh830@gmail.com>
2023-12-31 01:17:37 +0530

    updates

--- script.js
+++ script.js
@@ -12,56 +12,50 @@ const temperature = document.querySelector(".temperature");
 //   if (inputBox.value == "") {
 //     alert("Please enter the City!");
 //     return;
-//   } 
+//   }
 // }
 
 async function getWether(city) {
-    
   const Api_Key = "9c5c2063a22b2bb4373d1fd19da907fd";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_Key}`;
-  
 
-  const weatherData = await fetch(`${url}`).then((response) => response.json())
+  const weatherData = await fetch(`${url}`).then((response) => response.json());
   console.log(weatherData);
 
   if (inputBox.value == "") {
     alert("Please enter the City!");
     return;
-  } else{
-      currcity.innerHTML = `${weatherData.name}`;
+  } else {
+    currcity.innerHTML = `${weatherData.name}`;
   }
 
-  
   temperature.innerHTML = `${Math.round(weatherData.main.temp - 273.15)}°C`;
   humid.innerHTML = `${weatherData.main.humidity}%`;
   wind.innerHTML = `${weatherData.wind.speed} Km/h`;
-  // rain.innerHTML = `${weatherData.rain}% `;
-  weatherType.innerHTML =  `${weatherData.weather[0].main}`;
+  // rain.innerHTML = `${weatherData.rain['1h']}mm `;
+  weatherType.innerHTML = `${weatherData.weather[0].main}`;
 
-  switch(weatherData.weather[0].main){
-    case 'Clouds':
-        weatherImg.src = 'asset/cloudy1.png'
-        break;
-    case 'Rain':
-        weatherImg.src = 'asset/raining.png'
-        break;
-    case 'Sunny':
-        weatherImg.src = 'asset/sun.png'
-        break;
-    case 'Fog':
-        weatherImg.src = 'asset/fog1.png'
-        break;
-    case 'Mist':
-        weatherImg.src = 'asset/fog1.png'
-        break;
-    case 'Haze':
-        weatherImg.src = 'asset/fog1.png'
-        break;
+  switch (weatherData.weather[0].main) {
+    case "Clouds":
+      weatherImg.src = "asset/cloud.png";
+      break;
+    case "Rain":
+      weatherImg.src = "asset/raining.png";
+      break;
+    case "Sunny":
+      weatherImg.src = "asset/sun.png";
+      break;
+    case "Fog":
+      weatherImg.src = "asset/fog1.png";
+      break;
+    case "Mist":
+      weatherImg.src = "asset/fog1.png";
+      break;
+    case "Haze":
+      weatherImg.src = "asset/fog1.png";
+      break;
   }
-
-
 }
-
 
 btn.addEventListener("click", () => {
   getWether(inputBox.value);
